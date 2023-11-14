5e4d3d77d0dff077cdbc7c207d6097a6488d3a0c
alfredo <alfloz01@ucm.es>
2023-11-14 17:20:53 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '../src/styles.css';
+import { NumberKeyboard } from './keyboard/keyboard';
+
+function App() {
+  return (
+    <>
+      <NumberKeyboard></NumberKeyboard>
+    </>
+  );
+}
+
+export default App;
